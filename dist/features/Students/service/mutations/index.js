56e6f54dacf9c5812695c9288660ea60021b3782
joshuaasare <joshuaasare29@gmail.com>
2020-01-28 17:47:07 +0000

    removed dist

--- dist/features/Students/service/mutations/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./addStudentCompany"));
-__export(require("./registerStudent"));
-__export(require("./updateStudentCompany"));
-__export(require("./updateInternshipStart"));
